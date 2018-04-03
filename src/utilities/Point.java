c4d04cfa7dec593eb944f35ea00dba7362d16621
eliran gabay <34740857+EliranGabay@users.noreply.github.com>
2018-04-03 17:32:17 +0300

    final project update new with comment

--- src/utilities/Point.java
+++ src/utilities/Point.java
@@ -1,17 +1,29 @@
 package utilities;
-
+/**
+ * this class represent the Airplane as a racer with all needed components
+ * @version 3.4.2018
+ * @author Eliran gabay 203062831 & Linoy shriker 204027627
+ */
 public class Point {
     private double x;
     private double y;
     private final double MIN_X=0,MAX_X=20000;
 
+    /**
+     * this constructs a Point with a Location X,Y
+     * @param x Location double
+     * @param y Location double
+     */
     //constructor
     public Point(double x, double y)
     {
         setX(x);
         setY(y);
     }
-
+    /**
+     * this copy constructs a Point with a other Point
+     * @param other Point containing X,Y Location double
+     */
     //copy constructor
     public Point(Point other)
     {
@@ -19,6 +31,10 @@ public class Point {
         setY(other.getY());
     }
 
+    /**
+     * ALL SETTERS AND GETTERS PARAMETERS& RETURN VALUES ARE:
+     * X,Y
+     */
     public double getX() {
         return x;
     }
@@ -45,6 +61,9 @@ public class Point {
         return false;
     }
 
+    /**
+     * @return the string that contains all necessary info
+     */
     @Override
     public String toString()
     {
