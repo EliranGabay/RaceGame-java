c53069a35f9d20382fa56a5a5ad1aa0b750b23d7
eliran gabay <34740857+EliranGabay@users.noreply.github.com>
2018-04-21 18:07:43 +0300

    start project work 2 update first files

---
+++ src/utilities/EnumContainer.java
@@ -0,0 +1,20 @@
+package utilities;
+
+/**
+ * Contains all Enums for the game.
+ * 
+ * To set a field type: 
+ * 		EnumContainer.Vision vision;
+ * To set a value:
+ * 		this.vision = EnumContainer.Vision.Sunny
+ * 
+ * @author Bar Ohayon
+ *
+ */
+public class EnumContainer {
+
+	public static enum Vision {
+		CLOUDS, SUNNY, FOG
+	}
+
+}
