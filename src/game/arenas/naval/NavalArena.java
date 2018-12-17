ba768f7b77d39034b28112c3c3e868859436b332
eliran gabay <34740857+EliranGabay@users.noreply.github.com>
2018-12-17 09:32:18 +0200

    Update NavalArena.java

--- src/game/arenas/naval/NavalArena.java
+++ src/game/arenas/naval/NavalArena.java
@@ -1,7 +1,5 @@
 /**
  * this class represent the NavalArena as a Arena with all needed components
- * @version 29.42018
- * @author Eliran gabay 203062831
  */
 package game.arenas.naval;
 import game.arenas.Arena;
